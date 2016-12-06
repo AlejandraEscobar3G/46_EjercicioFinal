69be2a40d21ffe2b5f71e15cea4c92c8bd517acd
AlejandraEscobar3G <ale.fesico@gmail.com>
2016-12-06 11:02:37 -0600

    Etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
 	$('.js-menu').hide();
 	console.log('recipe.html listo');
+	$('.js-show-recipe').click(function(){
+		$('.page').removeClass('make');
+	});
+	$('.js-show-make').click(function(){
+		$('.page').addClass('make');
+	});
 });
